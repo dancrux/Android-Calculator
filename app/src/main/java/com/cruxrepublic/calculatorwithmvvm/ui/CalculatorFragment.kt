807ed1b30e84352146032ed826508838fa04b584
dancrux <nwogwugwudaniel@gmail.com>
2020-05-22 22:28:32 +0100

    Added more functionality for calc button pressed and viewModel setup

--- app/src/main/java/com/cruxrepublic/calculatorwithmvvm/ui/CalculatorFragment.kt
+++ app/src/main/java/com/cruxrepublic/calculatorwithmvvm/ui/CalculatorFragment.kt
@@ -6,15 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 
 import com.cruxrepublic.calculatorwithmvvm.R
 import com.cruxrepublic.calculatorwithmvvm.databinding.FragmentCalculatorBinding
-import kotlinx.android.synthetic.main.fragment_calculator.*
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.text.DecimalFormat
 
 /**
  * A simple [Fragment] subclass.
@@ -39,66 +39,129 @@ class CalculatorFragment : Fragment() {
         binding.historyButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_calculatorFragment_to_history2)
         }
-//number Buttons
-        binding.oneButton.setOnClickListener {    buttonPressed(true, "1") }
-        binding.twoButton.setOnClickListener {  buttonPressed(true, "2") }
-        binding.threeButton.setOnClickListener {   buttonPressed(true, "3") }
-        binding.fourButton.setOnClickListener {    buttonPressed(true, "4") }
-        binding.fiveButton.setOnClickListener {    buttonPressed(true, "5") }
-        binding.sixButton.setOnClickListener {   buttonPressed(true, "6") }
-        binding.sevenButton.setOnClickListener {     buttonPressed(true, "7") }
-        binding.eightButton.setOnClickListener {   buttonPressed(true, "8") }
-        binding.nineButton.setOnClickListener {   buttonPressed(true, "9") }
-        binding.zeroButton.setOnClickListener {  buttonPressed(true, "0") }
-        binding.dotButton.setOnClickListener {     buttonPressed(true, ".") }
-//        operation Buttons
-        binding.plusButton.setOnClickListener {    buttonPressed(true, "+") }
-        binding.minusButton.setOnClickListener {    buttonPressed(true, "-") }
-        binding.divideButton.setOnClickListener {     buttonPressed(true, "/") }
-        binding.multiplicationButton.setOnClickListener {   buttonPressed(true, "x") }
 
-        binding.clearButton.setOnClickListener { clear() }
-
-        binding.equalButton.setOnClickListener {    clear() }
-
-
-
-
+        initializeButtons()
       return  binding.root
     }
 
-    fun buttonPressed(clear: Boolean, string: String){
-        if (clear){
-            binding.result.text = ""
-            binding.inputText.append(string)
-        }else{
-            binding.inputText.append(binding.result.text)
-            binding.inputText.append(string)
-            binding.result.text = ""
-        }
-        }
 
-    fun clear(){
-        binding.inputText.text = ""
-        binding.result.text = ""
+
+    private fun initializeButtons(){
+        functionalButtons()
+        operationalButtons()
+        numericalButtons()
+
     }
 
-//    private fun numberPressed() {
+    private fun numericalButtons() {
+        //number Buttons
+        binding.oneButton.setOnClickListener {    displayAppendedDigit("1") }
+        binding.twoButton.setOnClickListener {  displayAppendedDigit("2")  }
+        binding.threeButton.setOnClickListener {  displayAppendedDigit("3") }
+        binding.fourButton.setOnClickListener {    displayAppendedDigit("4")  }
+        binding.fiveButton.setOnClickListener {   displayAppendedDigit("5")  }
+        binding.sixButton.setOnClickListener {   displayAppendedDigit("6")  }
+        binding.sevenButton.setOnClickListener {    displayAppendedDigit("7")  }
+        binding.eightButton.setOnClickListener {   displayAppendedDigit("8")  }
+        binding.nineButton.setOnClickListener {   displayAppendedDigit("9")  }
+        binding.zeroButton.setOnClickListener {  displayAppendedDigit("0")  }
+        binding.dotButton.setOnClickListener {    displayAppendedDigit(".") }
+
+    }
+
+    private fun operationalButtons() {
+        //        operation Buttons
+        binding.minusButton.setOnClickListener {
+//            viewModel.selectOperation('S')
+            displayAppendedDigit("-")
+
+        }
+        binding.plusButton.setOnClickListener {
+//            viewModel.selectOperation('A')
+            displayAppendedDigit("+")
+        }
+        binding.divideButton.setOnClickListener {
+//            viewModel.selectOperation('D')
+            displayAppendedDigit("/")
+        }
+        binding.multiplicationButton.setOnClickListener {
+//            viewModel.selectOperation('M')
+            displayAppendedDigit("x")
+        }
+
+    }
+
+    private fun functionalButtons() {
+//        handles the clear, backspace and equals button click
+        binding.clearButton.setOnClickListener { clear() }
+        binding.backSpaceButton.setOnClickListener { displayAfterClearDigit() }
+        binding.equalButton.setOnClickListener { performMathOperation() }
+
+    }
+
+    private fun displayAfterClearDigit(){
+        viewModel.clearDigit()
+        binding.inputText.text = viewModel.digitOnScreen.toString()
+        if (binding.result.text.isNotEmpty()){
+            clear()
+        }
+    }
+
+    private fun displayAppendedDigit(number: String){
+        viewModel.appendToDigit(number)
+        binding.inputText.text = viewModel.digitOnScreen.toString()
+
+    }
+    private fun performMathOperation(){
+
+//        viewModel.rightHandSide = viewModel.digitOnScreen.toString().toDouble()
 //
-//      var number = binding.oneButton.text
+//    when(viewModel.operation) {
+//        'A' -> {
+//            val sum = OperationsHelper.add(viewModel.leftHandSide,viewModel.rightHandSide)
+//            val df = DecimalFormat(".##")
+//            val formattedSum = df.format(sum)
+//            binding.result.text = formattedSum.toString()
+//            viewModel.digitOnScreen.append(formattedSum)
 //
-//        inputText.append(number)
-////        val button = view as Button
-////
-////        val number = button.text
+//        }
+//        'S' -> {
+//            val subtract = OperationsHelper.subtract(viewModel.leftHandSide, viewModel.rightHandSide)
+//            binding.result.text = subtract.toString()
+//            viewModel.digitOnScreen.append(subtract)
 //
-////            if (userIsTyping){
-////                result.append(number)
-////            }else{
-////                result.text =button.text
-////
-//////                userIsTyping = true
-////            }
+//        }
+//        'M' -> {
+//            val multiply = OperationsHelper.multiply(viewModel.leftHandSide, viewModel.rightHandSide)
+//            binding.result.text = multiply.toString()
+//            viewModel.digitOnScreen.append(multiply)
+//
+//        }
+//        'D' -> {
+//            val divide = OperationsHelper.divide(viewModel.leftHandSide, viewModel.rightHandSide)
+//            binding.result.text = divide.toString()
+//            viewModel.digitOnScreen.append(divide)
+//
+//        }
+//    }
+        val digit = binding.inputText.text.toString()
+        val expression = ExpressionBuilder(digit).build()
+
+        val result = expression.evaluate()
+        val df = DecimalFormat("#.#")
+        val formattedResult = df.format(result)
+
+//        val longResult = result.toLong()
+        binding.result.text = formattedResult.toString()
+    }
+
+   private fun clear(){
+        binding.inputText.text = ""
+        binding.result.text = ""
+       viewModel.digitOnScreen.clear()
+    }
+
+
     }
 
 
--- app/src/main/java/com/cruxrepublic/calculatorwithmvvm/ui/CalculatorViewModel.kt
+++ app/src/main/java/com/cruxrepublic/calculatorwithmvvm/ui/CalculatorViewModel.kt
@@ -5,64 +5,56 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import net.objecthunter.exp4j.Expression
+import java.lang.Exception
+import java.text.DecimalFormat
+
 
 class CalculatorViewModel:ViewModel() {
 
 
  private val tag = this::class.simpleName
+var digitOnScreen = StringBuilder()
+ var operation: Char =' '
+ var leftHandSide: Double = 0.0
+ var rightHandSide: Double = 0.0
+
 
  init {
-//  clearNumbers()
+//  clears the text input
+
+//  digitOnScreen.clear()
+
+
      Log.i("tag", "CalculatorViewModel created")
  }
 
+ fun clearDigit() {
+  val length = digitOnScreen.length
+     if (digitOnScreen.isNotEmpty()) {
+             digitOnScreen.deleteCharAt(length - 1)
+             leftHandSide = 0.0
+             rightHandSide = 0.0
+         }
+ }
+
+ fun selectOperation(c: Char){
+  operation = c
+     if (digitOnScreen.isNotEmpty()) {
+         leftHandSide = digitOnScreen.toString().toDouble()
+         digitOnScreen.clear()
+     }
+ }
+
+ fun appendToDigit(digit: String){
+  digitOnScreen.append(digit)
+ }
+//fun evaluateExpression(){
+//    Expression
+//}
  override fun onCleared() {
   super.onCleared()
   Log.i(tag, "CalculatorViewModel destroyed!")
  }
-
-
-
-//      for when the user is not done with input
-//    var userIsTyping = false
-//
-//    val operations = mapOf<String, (Double, Double) ->Double>(
-//        "+" to {a,b -> a + b},
-//        "-" to {a,b -> a - b},
-//        "/" to {a,b -> a / b},
-//        "x" to {a,b -> a * b}
-//    )
-//    data class PendingOperation(val firstNumber: Double ,val operation:(Double,Double) ->Double)
-//    var pendingOperation: PendingOperation? = null
-//    fun performPendingOperation(secondNumber: Double): Double{
-//        return pendingOperation?.operation!!.invoke(pendingOperation!!.firstNumber, secondNumber)
-//    }
-//
-////     function for displaying numbers when the numbers buttons are pressed
-//
-//    fun operationPerformed(view: View) {
-//        val button = view as Button
-//        if (result.text != ""){
-//            if (button.text != "="){
-//                pendingOperation = PendingOperation(result.text.toString().toDouble(), operations[button.text]!!)
-//                result.text =""
-//            }
-//        }else{
-////            handles the pending operation
-//            result.text = performPendingOperation(result.text.toString().toDouble()).toString()
-////            pendingOperation = null
-//
-//
-//        }
-//    }
-////   clears the text in the display
-//    fun clearNumbers(view: View) {
-//        if (userIsTyping){
-//            result.text =""
-//        }
-//
-//    }
-
-
 
 }
